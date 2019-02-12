b64c2392cf6d5498ed44a68ca0ef75cd35c172ea
Tomaž <cofek0@gmail.com>
2019-02-12 18:31:13 +0100

    fixed adding comments
    * check if voted for all
    * login required
    * if user is owner can add comments

--- commentality/comment/views.py
+++ commentality/comment/views.py
@@ -16,24 +16,39 @@ def create():
   if error:
     return custom_response(error, 400)
 
-  # Create and save comment
-  comment = Comment(contents=data['contents'])
-  comment.save()
+  article_uid = data['article_uid']
+  article = Article.get(article_uid)
+  if not article:
+    return custom_response({'error': 'Article does not exists.'}, 404)
 
-  # Connect owner
+  if not article.can_comment:
+    return custom_response({'error': 'commenting locked'}, 400)
+
   owner_id = g.user.get('uid')
   owner = User.get(owner_id)
-  comment.owner.connect(owner)
+  if not owner:
+    return custom_response({'error': 'Only registered users can comment.'}, 400)
+  if article.comments.filter(owner==owner):
+    return custom_response({'error': 'You have already commented on this article.'}, 400)
 
-  article_external_id = data['article_external_id']
-  article = Article.get_by_external_id(article_external_id)
-  if not article:
-    article = Article(external_id=article_external_id)
-    article.save()
-  comment.article.connect(article)
+  # Check if is user owner of article or user voted for all other comments of article
+  total_comments = article.comments.count()
+  voted_comments = owner.votes.filter(article=article).count()
 
-  data = comment_schema.dump(comment).data
-  return custom_response(data, 201)
+  if article.owner == owner or total_comments == voted_comments:
+    # Create and save comment
+    comment = Comment(contents=data['contents'])
+    comment.save()
+
+    comment.article.connect(article)
+
+    # Connect owner
+    comment.owner.connect(owner)
+
+    data = comment_schema.dump(comment).data
+    return custom_response(data, 201)
+  else:
+    return custom_response({'error': 'first, vote for all comments'}, 400)
 
 @blueprint.route('/', methods=['GET'])
 def get_all():
@@ -87,6 +102,9 @@ def vote(comment_id):
   comment = Comment.get(comment_id)
   if not comment:
     return custom_response({'error': 'comment not found'}, 404)
+
+  #if not comment.article.can_vote:
+  #  return custom_response({'error': 'voting locked'}, 400)
 
   voter_id = g.user.get('uid')
   voter = User.get(voter_id)
